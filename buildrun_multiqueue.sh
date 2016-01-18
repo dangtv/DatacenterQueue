7a4677aa91a940f1a28133c4e03aff25c3a76011
Dang Tran <dangtv18@gmail.com>
2016-01-18 12:01:16 +0000

    add to results experiment time off -> middle = 400 the other threshold -0.8

--- buildrun_multiqueue.sh
+++ buildrun_multiqueue.sh
@@ -1,4 +1,5 @@
 javac -d bin -sourcepath src -cp libs/cloudsim-3.0.3.jar src/cmsfinitequeuenostag/CFQNSMain.java;
 
-java -cp bin:libs/cloudsim-3.0.3.jar cmsfinitequeuenostag.CFQNSMain 0.2 0.002 50 0.2 0.8;
-java -cp bin:libs/cloudsim-3.0.3.jar cmsfinitequeuenostag.CFQNSMain 0.2 0.002 100 0.2 0.8;
+java -cp bin:libs/cloudsim-3.0.3.jar cmsfinitequeuenostag.CFQNSMain 0.2 0.002 50 0.2 -0.8;
+java -cp bin:libs/cloudsim-3.0.3.jar cmsfinitequeuenostag.CFQNSMain 0.2 0.002 100 0.2 -0.8;
+java -cp bin:libs/cloudsim-3.0.3.jar cmsfinitequeuenostag.CFQNSMain 0.2 0.002 200 0.2 -0.8;
